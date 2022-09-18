6961703f4d150edd5ff0ff06c6c4a2eeab49ab08
Takehiro Yokoyama <rha.t.4580@gmail.com>
2022-09-18 16:15:56 +0900

    add join link in login screen

--- components/CustomLoginScreen.js
+++ components/CustomLoginScreen.js
@@ -2,45 +2,39 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, Linking } from "react-native";
 import LoginScreen from "@src/containers/Custom/LoginScreen";
 import AppButton from "@src/components/AppButton";
-import { getExternalCodeSetup } from '@src/externalCode/externalRepo';
-
-const externalCodeSetup = getExternalCodeSetup();
-
-// console.log(LoginScreen.hideTitle);
-
 
 const CustomLoginScreen = (props) => {
-  // LoginScreen.hideTitle = true;
-  // props.hid
-  
-  return <>
+  return <View style={{flex: 1}}>
     <LoginScreen {...props} />
     <View style={styles.textcontainer}>
-      <Text style={styles.text} onPress={() => Linking.openURL('https://google.com')}>掲載をご希望の方はこちら</Text>
+      <Text style={styles.text} onPress={() => Linking.openURL('https://re-besty.com/join')}>掲載をご希望の方はこちら</Text>
     </View>
-  </>
+  </View>
 }
+
+CustomLoginScreen.navigationOptions = {header: null};
 
 export default CustomLoginScreen;
 
 const styles = StyleSheet.create({
   textcontainer: {
     // flex: 1,
-    height: 60,
+    height: 80,
     // paddingTop: 5,
     // paddingBottom: 5,
     paddingLeft: 5,
     paddingRight: 5,
+    backgroundColor: '#1E42DD',
     alignContent: 'center',
-    justifyContent: 'center',
-    textAlign: 'center'
+    justifyContent: 'center'
   },
   text: {
     // flex: 1,
-    height: 60,
-    alignSelf: 'stretch',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // height: 60,
+    // alignSelf: 'stretch',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    color: '#fff',
     textAlign: 'center',
     textDecorationLine: 'underline'
   }
--- index.js
+++ index.js
@@ -3,6 +3,13 @@ import React from "react";
 import {Text, TextInput, View} from "react-native";
 import CustomLoginScreen from "./components/CustomLoginScreen";
 
-export const applyCustomCode = (externalCodeSetup: any) => {
-	externalCodeSetup.navigationApi.replaceScreenComponent("LoginScreen", CustomLoginScreen);
+export const applyCustomCode = externalCodeSetup => {
+  externalCodeSetup.navigationApi.replaceScreenComponent("LoginScreen", CustomLoginScreen);
+
+  // externalCodeSetup.profileScreenHooksApi.setAfterProfileHeader(
+  //   props => {
+  //     console.log(props);
+  //     return <Text>showing user id after header, {props.user.id}</Text>
+  //   }
+  // ); 
 };
